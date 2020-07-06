0845945f3c3c6febba123f3e56e5bbfccaddbd82
LoicAuge <loic.auge@limayrac.fr>
2020-07-06 18:00:24 +0200

    list élèves on cursus

--- school/lycee/urls.py
+++ school/lycee/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<id>[0-9]+)$', views.detail, name='detail')
+    url(r'^(?P<idC>[0-9]+)$', views.detail, name='detail')
 ]
--- school/lycee/views.py
+++ school/lycee/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from .models import Cursus
+from .models import Cursus, Student
 from django.template import loader
 
 def index(request):
@@ -10,6 +10,8 @@ def index(request):
     context = {'liste': result_list}
     return render(request, 'lycee/index.html', context)
 
-def detail(request, id):
-    resp = "resultat pour cursus {}".format(id)
-    return HttpResponse(resp)
+def detail(request, idC):
+    result_list = Student.objects.filter(cursus_id = idC)
+    cursus = Cursus.objects.get(id=idC)
+    context = {'liste': result_list, 'cursusName': cursus.name}
+    return render(request, 'lycee/list-onCursus.html', context)
