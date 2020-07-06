7b8acf4a31a050022100f0ac2e2dbb4046108108
LoicAuge <loic.auge@limayrac.fr>
2020-07-06 12:58:44 +0200

    views

--- school/lycee/views.py
+++ school/lycee/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from .models import Cursus
+from django.template import loader
+
+def index(request):
+    result_list = Cursus.objects.order_by('name')
+    context = {'liste': result_list}
+    return render(request, 'lycee/index.html', context)
+
+def detail(request, id):
+    resp = "resultat pour cursus {}".format(id)
+    return HttpResponse(resp)
