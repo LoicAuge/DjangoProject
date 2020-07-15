93c7968a0332a8da2dcf44b208e5ef2666c0f05f
LoicAuge <loic.auge@limayrac.fr>
2020-07-15 12:35:00 +0200

    Forms creation + update student

--- school/lycee/urls.py
+++ school/lycee/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<idC>[0-9]+)$', views.detail, name='detail')
+    url(r'^(?P<idC>[0-9]+)$', views.detail, name='detail'),
+    url(r'^student/create/', views.StudentCreateView.as_view(), name='create_eleve'),
+    url(r'^student/update/(?P<pk>[0-9]+)$', views.StudentUpdateView.as_view(), name='update_eleve'),
+    url(r'^student/(?P<idS>[0-9]+)$', views.detail_Student, name='detail_student')
 ]
--- school/lycee/views.py
+++ school/lycee/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.urls import reverse
+from django.views.generic import CreateView, UpdateView
 from .models import Cursus, Student
+from .forms import StudentForm
 from django.template import loader
 
 def index(request):
@@ -15,3 +18,24 @@ def detail(request, idC):
     cursus = Cursus.objects.get(id=idC)
     context = {'liste': result_list, 'cursusName': cursus.name}
     return render(request, 'lycee/list-onCursus.html', context)
+
+class StudentCreateView(CreateView):
+    model = Student
+    form_class = StudentForm
+    template_name = "lycee/student/create.html"
+
+    def get_success_url(self):
+        return reverse("detail_student", args=(self.object.pk,))
+
+def detail_Student(request, idS):
+    student = Student.objects.get(id = idS)
+    context = {'eleve': student}
+    return render(request, 'lycee/student/detail_eleve.html', context)
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    form_class = StudentForm
+    template_name = "lycee/student/create.html"
+
+    def get_success_url(self):
+        return reverse("detail_student", args=(self.object.pk,))
